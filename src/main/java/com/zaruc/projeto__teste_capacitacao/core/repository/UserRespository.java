083fda1333ef2db30290281e010e1136a261d534
Ronicley Angelo <ronicley8@hotmail.com>
2024-01-27 15:14:37 -0300

    Pegando A role do usuário

--- src/main/java/com/zaruc/projeto__teste_capacitacao/core/repository/UserRespository.java
+++ src/main/java/com/zaruc/projeto__teste_capacitacao/core/repository/UserRespository.java
@@ -3,8 +3,14 @@ package com.zaruc.projeto__teste_capacitacao.core.repository;
 import com.zaruc.projeto__teste_capacitacao.core.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.security.core.userdetails.UserDetails;
 
 public interface UserRespository extends JpaRepository<User, Long>, JpaSpecificationExecutor<User> {
-    UserDetails findByUsername(String username);
+    @Query("SELECT u FROM User u " +
+            "JOIN FETCH u.roles " +
+            "WHERE u.username = :login")
+    User findByUsername(@Param("login") String login);
+//    UserDetails findByUsername(String username);
 }
